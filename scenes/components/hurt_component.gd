01a4b51ce54a159edfa5017a850af852ba557627
mario <mario_reyes505@hotmail.com>
2024-11-24 14:40:16 -0500

    created empy hit, hurt components

---
+++ scenes/components/hurt_component.gd
@@ -0,0 +1,10 @@
+class_name HurtComponent
+
+extends Area2D
+
+@export var tool: DataTypes.tools = DataTypes.tools.None
+
+signal on_hurt
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
