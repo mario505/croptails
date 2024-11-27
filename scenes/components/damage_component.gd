cd3bdd4b46fdfc9dc1ff8ca7b6d89d8fc1e6ed3b
mario <mario_reyes505@hotmail.com>
2024-11-27 00:29:19 -0500

    added base logic for hit, hurt comp, added them to player and trees

---
+++ scenes/components/damage_component.gd
@@ -0,0 +1,2 @@
+class_name DamageComponent
+extends Node2D
--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -7,4 +7,7 @@ extends Area2D
 signal on_hurt
 
 func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	var hit_component = area as HitComponent
+	
+	if tool == hit_component.current_tool:
+		on_hurt.emit(hit_component.hit_damage)
