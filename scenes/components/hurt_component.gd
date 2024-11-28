0b3b159fd02b3299145400457d5bda151dd4c6c5
mario <mario_reyes505@hotmail.com>
2024-11-28 06:20:42 -0500

    created damage comp, log object (empty)

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -4,10 +4,10 @@ extends Area2D
 
 @export var tool: DataTypes.tools = DataTypes.tools.None
 
-signal on_hurt
+signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
 	
 	if tool == hit_component.current_tool:
-		on_hurt.emit(hit_component.hit_damage)
+		hurt.emit(hit_component.hit_damage)
