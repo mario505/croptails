ec0b8779886a4c1c274cd1f338f99a6e59680852
mario <mario_reyes505@hotmail.com>
2024-12-05 20:28:21 -0500

    created empty chicken scene

---
+++ scenes/characters/chicken/idle_state.gd
@@ -0,0 +1,19 @@
+extends NodeState
+
+@export var character :CharacterBody2D
+@export var animated_sprite_2e :AnimatedSprite2D
+
+func _on_process(_delta :float) -> void:
+	pass
+
+func _on_physics_process(_delta :float) -> void:
+	pass
+	
+func _on_next_transitions() -> void:
+	pass
+	
+func _on_enter() -> void:
+	pass
+	
+func _on_exit() -> void:
+	pass
