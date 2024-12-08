1d70d2ad67faeb6cdcbc1d5a444cc8fd6d9f2347
mario <mario_reyes505@hotmail.com>
2024-12-08 09:32:39 -0500

    added idle animation for chicken, added state machine

--- scenes/characters/chicken/walk_state.gd
+++ scenes/characters/chicken/walk_state.gd
@@ -2,6 +2,9 @@ extends NodeState
 
 @export var character :CharacterBody2D
 @export var animated_sprite_2d :AnimatedSprite2D
+@export var idle_state_time_interval :float = 5.0
+
+@onready var idle_state_time :Timer = Timer.new()
 
 func _on_process(_delta :float) -> void:
 	pass
@@ -13,7 +16,7 @@ func _on_next_transitions() -> void:
 	pass
 	
 func _on_enter() -> void:
-	animated_sprite_2d.play("idle")
+	pass
 	
 func _on_exit() -> void:
-	animated_sprite_2d.stop()
+	pass
