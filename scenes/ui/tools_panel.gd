f0e06ab472ef01ddc445452e04af90cd4f4f711c
mario <mario_reyes505@hotmail.com>
2024-12-22 13:01:42 -0500

    finished tools panel, created empty tool manager script

---
+++ scenes/ui/tools_panel.gd
@@ -0,0 +1,11 @@
+extends PanelContainer
+
+@onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
+@onready var tool_tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
+@onready var tool_watering_can: Button = $MarginContainer/HBoxContainer/ToolWateringCan
+@onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
+@onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
+
+
+func _on_tool_axe_pressed() -> void:
+	pass # Replace with function body.
